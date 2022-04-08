5d08961c02b345d2e50a49eae9b78d9a460d3e49
gobblygobble <gobblygobble@kaist.ac.kr>
2022-04-09 02:32:20 +0900

    print out stats at the end

--- output_parser.py
+++ output_parser.py
@@ -3,7 +3,7 @@ import sys
 filename = sys.argv[1]
 
 def main():
-    print(filename)
+    print(f"Printing out parsing results for: {filename}")
     f = open(filename, "r")
     rl = f.readlines()
     non_debug_lines = []
@@ -25,23 +25,49 @@ def main():
             # infiltrated
             d = dict()
             m = l.split()
-            d["slams"] = m[7]
-            d["resets"] = m[10]
+            d["slams"] = int(m[7])
+            d["resets"] = int(m[10])
             fail_list.append(d)
         elif l.startswith("The apartment has not been infiltrated,"):
             # not infiltrated
             d = dict()
             m = l.split()
-            d["slams"] = m[8]
-            d["resets"] = m[11]
+            d["slams"] = int(m[7])
+            d["resets"] = int(m[10])
             success_list.append(d)
         else:
             print(f"!!!!! Unknown line parsing {filename}: {l}")
 
-    print("success list:")
-    print(success_list)
-    print("fail list:")
-    print(fail_list)
+    #print("success list:")
+    #print(success_list)
+    #print("fail list:")
+    #print(fail_list)
+
+    F = len(fail_list)
+    S = len(success_list)
+    print(f"Success: {S} / Total: {F + S}, success rate: {S / (F + S):.2f}")
+
+    avg_success_reset = get_average(success_list, "resets")
+    avg_fail_slam = get_average(fail_list, "slams")
+    
+    if avg_success_reset:
+        print(f"In case of success, there was an average of {avg_success_reset:.2f} resets per run")
+    else:
+        print("No successful runs")
+
+    if avg_fail_slam:
+        print(f"In case of failure, the apartment managed to tank {avg_fail_slam:.2f} slams")
+    else:
+        print("No failed runs")
+
+def get_average(l, key):
+    if len(l) == 0:
+        return None
+    sum = 0
+    for elem in l:
+        sum += elem[key]
+    return sum / len(l)
+
 
 if __name__ == "__main__":
     main()
