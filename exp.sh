2af76c0750d8c13eb8194906a1e20fefb50b4c3c
gobblygobble <gobblygobble@kaist.ac.kr>
2022-04-09 03:22:07 +0900

    experiment-ready

--- exp.sh
+++ exp.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-filename="temp_results.txt"
+#filename="temp_results.txt"
+#rm $filename
+#for i in {1..10}; do
+#    python3 main.py >> $filename 2> /dev/null
+#    #python3 main.py >> $filename
+#done
+#python3 output_parser.py $filename
 
-rm $filename
-for i in {1..10}; do
-    python3 main.py >> $filename 2> /dev/null
-    #python3 main.py >> $filename
+
+mkdir -p out
+for at in "per_apartment" "per_floor" "per_line" "per_door"; do
+    for lm in "uniform" "two_quadrants" "few_hot" "gaussian"; do
+        filename="./out/"$at"__"$lm".log"
+        for it in {1..100}; do
+            python3 main.py $at $lm >> $filename 2> /dev/null
+            #python3 main.py $at $lm >> $filename
+            #echo "" > /dev/null
+        done
+        #echo "python3 main.py $at $lm >> $filename 2> /dev/null"
+        python3 output_parser.py $filename
+        #echo "python3 output_parser.py $filename"
+    done
 done
-python3 output_parser.py $filename
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import sys
 
 import security_guard as sg
 import random_access_generator as rag
@@ -9,26 +10,47 @@ logger.setLevel(logging.DEBUG)
 #logger.setLevel(logging.WARNING)
 
 def main():
-    print("******************** [Begin secure apartment simulation] ********************")
-    
-    print("******************** [ End secure apartment simulation ] ********************")
-
-def test():
-    num_slams = 500
-    nf = 6
-    nl = 6
-    th = 5
-    lrt = 20
+    num_slams = 100000
+    nf = 20
+    nl = 10
+    th = 10
+    lrt = 10
+    tradeoff_var = 4 / 5
     db = True
     p = dict()
     p["num_floors"] = nf
     p["num_lines"] = nl
-    p["hot_percentage"] = 5
+    p["hot_percentage"] = 10
     
     # PER_APARTMENT, PER_FLOOR, PER_LINE, PER_DOOR
-    lm = sg.PER_APARTMENT
+    if sys.argv[1] == "per_apartment":
+        lm = sg.PER_APARTMENT
+        th = int(th * nf * nl * tradeoff_var * tradeoff_var)
+    elif sys.argv[1] == "per_floor":
+        lm = sg.PER_FLOOR
+        th = int(th * nf * tradeoff_var)
+    elif sys.argv[1] == "per_line":
+        lm = sg.PER_LINE
+        th = int(th * nl * tradeoff_var)
+    elif sys.argv[1] == "per_door":
+        lm = sg.PER_DOOR
+        th = int(th)
+    else:
+        print(f"!!!!! Wrong logging method argument: {sys.argv[1]}")
+        exit(-1)
+
     # UNIFORM, TWO_QUADRANTS, FEW_HOT, PSEUDOGAUSSIAN_CENTERED
-    ap = rag.PSEUDOGAUSSIAN_CENTERED
+    if sys.argv[2] == "uniform":
+        ap = rag.UNIFORM
+    elif sys.argv[2] == "two_quadrants":
+        ap = rag.TWO_QUADRANTS
+    elif sys.argv[2] == "few_hot":
+        ap = rag.FEW_HOT
+    elif sys.argv[2] == "gaussian":
+        ap = rag.PSEUDOGAUSSIAN_CENTERED
+    else:
+        print(f"!!!!! Wrong access pattern argument: {sys.argv[2]}")
+        exit(-1)
 
     guard = sg.SecurityGuard(num_floors = nf,
                              num_lines = nl,
@@ -42,12 +64,10 @@ def test():
     for i in range(num_slams):
         location = gen.generate_random_access()
         f, l = location["floor"], location["line"]
-        #f, l = random.randint(0, nf - 1), random.randint(0, nl - 1)
         logging.debug(f"Slamming #{i}: household[{f}][{l}]")
         if not guard.slam_door(floor = f, line = l):
             break
     guard.print_status()
 
 if __name__ == '__main__':
-    test()
-    #main()
+    main()
