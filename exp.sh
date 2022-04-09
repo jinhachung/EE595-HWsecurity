cadd6ca721536a5664142e8a7a49e87f3fe3d8eb
Jinha Chung <jinha.chung@kaist.ac.kr>
2022-04-09 10:57:35 +0900

    save parsed output to file

--- exp.sh
+++ exp.sh
@@ -9,17 +9,23 @@
 #python3 output_parser.py $filename
 
 
+rm -rf out/raw
+rm -rf out/parsed
 mkdir -p out
+mkdir -p out/raw
+mkdir -p out/parsed
 for at in "per_apartment" "per_floor" "per_line" "per_door"; do
     for lm in "uniform" "two_quadrants" "few_hot" "gaussian"; do
-        filename="./out/"$at"__"$lm".log"
-        for it in {1..100}; do
-            python3 main.py $at $lm >> $filename 2> /dev/null
-            #python3 main.py $at $lm >> $filename
+        filename=$at"__"$lm".log"
+        rawname="./out/raw/"$filename
+        parsedname="./out/parsed/"$filename
+        for it in {1..10}; do
+            python3 main.py $at $lm >> $rawname 2> /dev/null
+            #python3 main.py $at $lm >> $rawname
             #echo "" > /dev/null
         done
-        #echo "python3 main.py $at $lm >> $filename 2> /dev/null"
-        python3 output_parser.py $filename
-        #echo "python3 output_parser.py $filename"
+        #echo python3 main.py $at $lm >> $rawname 2> /dev/null"
+        python3 output_parser.py $rawname > $parsedname
+        #echo "python3 output_parser.py $filename > $parsedname"
     done
 done
